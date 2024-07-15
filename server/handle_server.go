30af41d244cec4d485635bbb4b5d579e54a566f6
fanmo-yun <xiaomojun9527@outlook.com>
2024-07-15 19:56:41 +0800

    feat: add shutdown

--- server/handle_server.go
+++ server/handle_server.go
@@ -1,10 +1,15 @@
 package server
 
 import (
+	"context"
 	"fmt"
 	"gohttp/logger"
 	"gohttp/utils"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"go.uber.org/zap"
 )
@@ -19,9 +24,29 @@ func ServerRun() {
 		Handler: HandleRouter(config),
 	}
 
-	zap.L().Info("Server Running", zap.String("http", address))
-	err := gohttp.ListenAndServe()
-	if err != nil {
-		zap.L().Fatal("Listen And Serve Fatal", zap.Error(err))
+	go func() {
+		zap.L().Info("Server Running", zap.String("http", address))
+		err := gohttp.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			zap.L().Fatal("Listen And Serve Fatal", zap.Error(err))
+		}
+	}()
+
+	ShutdownServer(&gohttp)
+}
+
+func ShutdownServer(s *http.Server) {
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+	<-quit
+	zap.L().Info("Server is shutting down...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := s.Shutdown(ctx); err != nil {
+		zap.L().Fatal("Server forced to shutdown", zap.Error(err))
 	}
+
+	zap.L().Info("Server exiting")
 }
