6dafa3b8bf7aa2f5564fab2a70d61721d153a42a
fanmo-yun <xiaomojun9527@outlook.com>
2024-07-16 10:53:18 +0800

    feat: add banner cannot find

--- banner/ban.go
+++ banner/ban.go
@@ -9,7 +9,7 @@ func ShowBanner() {
 	ban, readErr := os.ReadFile("banner.txt")
 	if readErr != nil {
 		fmt.Fprintf(os.Stderr, "gohttp: Cannot read banner.txt: %v\n", readErr)
-		os.Exit(1003)
+		return
 	}
 	fmt.Println(string(ban) + "\n")
 }
