bfc75808059dbe933549b969e2542d9b3ae61142
fanmo-yun <xiaomojun9527@outlook.com>
2024-11-14 15:40:13 +0800

    feat: update banner and config

--- banner/ban.go
+++ banner/ban.go
@@ -8,7 +8,7 @@ import (
 func ShowBanner() {
 	ban, readErr := os.ReadFile("banner.txt")
 	if readErr != nil {
-		fmt.Fprintf(os.Stderr, "gohttp: Cannot read banner.txt: %v\n", readErr)
+		fmt.Fprintf(os.Stderr, "gohttp: %v\n", readErr)
 		return
 	}
 	fmt.Println(string(ban) + "\n")
